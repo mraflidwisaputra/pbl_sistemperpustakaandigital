352028576bd511615f299594c933b2344c6daad1
ryanthimothy184ux <ryanthimothy184@gmail.com>
2026-04-07 21:31:33 +0700

    menambahkan fitur kelola data dan kelola denda

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/pinjambuku', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::get('/kelola data', function () {
+    return view('keloladata');
+});
+
+Route::get('/keloladenda', function () {
+    return view('keloladenda');
+});
